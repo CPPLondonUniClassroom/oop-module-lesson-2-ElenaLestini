cc60fe094174a54134c7ea55c74429ee26797b34
Oliver Smith <oliver@uptheinter.net>
2019-02-18 23:47:33 +0000

    OOP Lesson 2 - Solution

--- EXERCISE.h
+++ EXERCISE.h
@@ -2,10 +2,20 @@
 
 //This is for part 1
 class ILogger {
-
+public:
+    virtual ILogger& LogInfo(const std::string&) = 0;
+    virtual ILogger& LogError(const std::string&) = 0;
+    virtual const std::string& GetLastError() const = 0;
+    virtual ~ILogger() = default;
 };
 
 // This is for Part 2/3
-class StringLogger {
-
+class StringLogger : public ILogger {
+    std::ostream& o;
+    std::string lastError;
+public:
+    StringLogger(std::ostream& o) : o{o} {}
+    ILogger& LogInfo(const std::string& s) override { o << "INFO: " << s; return *this; }
+    ILogger& LogError(const std::string& s) override { o << "ERROR: " << s; lastError = s; return *this; }
+    const std::string& GetLastError() const override { return lastError; }
 };
